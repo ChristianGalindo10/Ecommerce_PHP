2b544d82577f78ee5b24575fc081be983d812fe0
ChristianGalindo10 <cygc1@hotmail.com>
2020-03-27 18:54:39 -0500

    Finalizando

--- configs/funciones.php
+++ configs/funciones.php
@@ -1,23 +1,12 @@
 <?php
 include "configs/abrirBD";
-/*
-$host_mysql = "localhost";
-$user_mysql = "root";
-$pass_mysql = "";
-$db_mysql = "tienda";
-$mysqli = mysqli_connect($host_mysql,$user_mysql,$pass_mysql,$db_mysql);
-*/
+
 
 function clear($var){
 	htmlspecialchars($var);
 	return $var;
 }
 
-function check_admin(){
-	if(!isset($_SESSION['id'])){
-		redir("./");
-	}
-}
 
 function redir($var){
 	?>
@@ -66,40 +55,8 @@ EOF;
 $ret = $db->query($sql);
 $row = $ret->fetchArray(SQLITE3_ASSOC);
 return $row['n_username'];
-/*
-	$q = $mysqli->query("SELECT * FROM clientes WHERE id = '$id_cliente'");
-	$r = mysqli_fetch_array($q);
-	return $r['name'];*/
 }
 
-function connect(){
-	$host_mysql = "localhost";
-	$user_mysql = "root";
-	$pass_mysql = "";
-	$db_mysql = "tienda";
-
-
- 	$mysqli = mysqli_connect($host_mysql,$user_mysql,$pass_mysql,$db_mysql);
-
-	return $mysqli;
-}
-
-function fecha($fecha){
-	$e = explode("-",$fecha);
-
-	$year = $e[0];
-	$month = $e[1];
-	$e2 = explode(" ",$e[2]);
-	$day = $e2[0];
-	$time = $e2[1];
-
-	$e3 = explode(":",$time);
-	$hour = $e3[0];
-	$mins = $e3[1];
-
-	return $day."/".$month."/".$year." ".$hour.":".$mins;
-
-}
 
 function estado($id_estado){
 		if($id_estado == 0){
@@ -118,31 +75,4 @@ function estado($id_estado){
 
 }
 
-function admin_name_connected(){
-	include "config.php";
-	$id = $_SESSION['id'];
-	$mysqli = connect();
-
-	$q = $mysqli->query("SELECT * FROM admins WHERE id = '$id'");
-	$r = mysqli_fetch_array($q);
-
-	return $r['name'];
-
-}
-
-function estado_pago($estado){
-
-	if($estado==0){
-		$estado = "Sin Verificar";
-	}elseif($estado==1){
-		$estado = "Verificado y Aprobado";
-	}elseif($estado==2){
-		$estado = "Reembolsado";
-	}else{
-		$estado = "Sin Verificar";
-	}
-
-	return $estado;
-
-}
 ?>
